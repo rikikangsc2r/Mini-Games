5398869784c427a03764d855d0aa22307aeb35b8
rikikangsc2r <ifannydarat55@gmail.com>
2025-09-09 11:51:08 +0700

    feat: Add Drawing game and Fabric.js integration
    Integrates a new "Drawing" game mode to NkGame, enabling collaborative drawing experiences. This involves:
    
    - Including the Fabric.js library for canvas manipulation.
    - Defining new types for the drawing game state.
    - Adding a `DrawingIcon` component for the game menu.
    - Implementing the `DrawingGame` component, which utilizes Fabric.js for drawing on a canvas.
    - Storing drawing paths in Firebase Realtime Database.
    - Updating Firebase security rules to accommodate the new `drawing-games` path and its structure.

--- types.ts
+++ types.ts
@@ -1,3 +1,3 @@
 export type Player = 'X' | 'O';
-export type GameID = 'menu' | 'tictactoe' | 'gobblet' | 'chess' | 'connect4' | 'blog' | 'stats';
+export type GameID = 'menu' | 'tictactoe' | 'gobblet' | 'chess' | 'connect4' | 'blog' | 'stats' | 'drawing';
 export type GameMode = 'menu' | 'local' | 'online' | 'ai';
